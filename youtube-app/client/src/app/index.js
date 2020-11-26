ca1e142d62ac1de5735d6a8f17b6171a194d2391
Cristina Lawson <30380172+cristina95138@users.noreply.github.com>
2020-11-25 18:29:07 -0800

    Updates

--- youtube-app/client/src/app/index.js
+++ youtube-app/client/src/app/index.js
@@ -1,16 +1,17 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, useLocation } from 'react-router-dom'
 
 import { NavBar } from '../components'
-import { MoviesList, MoviesInsert, MoviesUpdate } from '../pages'
+import { MoviesList, MoviesInsert, MoviesUpdate, Login, CreateAccount } from '../pages'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-function App() {
+function App()  {
     return (
         <Router>
             <NavBar />
             <Switch>
+                <Route path="/" exact component={Login} exact/>
                 <Route path="/movies/list" exact component={MoviesList} />
                 <Route path="/movies/create" exact component={MoviesInsert} />
                 <Route
--- youtube-app/client/src/pages/index.js
+++ youtube-app/client/src/pages/index.js
@@ -1,5 +1,7 @@
 import MoviesList from './MoviesList'
 import MoviesInsert from './MoviesInsert'
 import MoviesUpdate from './MoviesUpdate'
+import Login from './Login'
+import CreateAccount from './CreateAccount'
 
-export { MoviesList, MoviesInsert, MoviesUpdate }
+export { MoviesList, MoviesInsert, MoviesUpdate, Login, CreateAccount }
