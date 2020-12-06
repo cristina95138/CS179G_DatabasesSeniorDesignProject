ffe441197e059c754ade384f040412dec654eb52
jtran094 <jtran094@ucr.edu>
2020-12-05 22:21:55 -0800

    added user ctrl functions

---
+++ youtube-app/server/routes/user-router.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const UserCtrl = require('../controllers/user-ctrl')
+
+const router = express.Router()
+
+router.post('/user', UserCtrl.createUser)
+router.delete('/user/:id', UserCtrl.deleteUser)
+router.get('/user/:id', UserCtrl.getUserById)
+router.get('/users', UserCtrl.getUsers)
+router.get('/users', UserCtrl.getRecommend)
+router.put('/user/:id', UserCtrl.setPref)
+
+module.exports = router
